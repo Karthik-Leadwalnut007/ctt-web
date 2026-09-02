f05604d8c2e3304690bb9527d0b8838da03bf239
Karthik-Leadwalnut007 <karthiks31@gmail.com>
2026-09-02 19:41:00 +0530

    Move OAuth routes to /auth and /callback for Decap CMS

---
+++ app/callback/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const code = req.nextUrl.searchParams.get("code")
+
+  if (!code) {
+    return new NextResponse("Missing code", { status: 400 })
+  }
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  })
+
+  const data = await response.json()
+  const token = data.access_token
+
+  if (!token) {
+    return new NextResponse("Failed to get token", { status: 500 })
+  }
+
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <script>
+      (function() {
+        function receiveMessage(e) {
+          window.opener.postMessage(
+            'authorization:github:success:{"token":"${token}","provider":"github"}',
+            e.origin
+          );
+        }
+        window.addEventListener("message", receiveMessage, false);
+        window.opener.postMessage("authorizing:github", "*");
+      })()
+    </script>
+    </body>
+    </html>
+  `
+
+  return new NextResponse(html, {
+    headers: { "Content-Type": "text/html" },
+  })
+}
